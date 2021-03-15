7b3e840d5af8ec7c7ec996b9d48874720fd45f69
espere-papillon <marinabov007@gmail.com>
2021-03-15 22:59:51 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let chars = str.split(''),
+        stack = [],
+        open = bracketsConfig.map(i => i[0]),
+        close = bracketsConfig.map(i => i[1]),
+        closeIndex,
+        openIndex,
+        count = 0,
+        arr = [];
+
+    for (let i = 0; i<open.length;i++)
+        arr.push(0);
+
+    // Проходимся по строке, проверяя каждый ее символ (п.4).
+    for (let i = 0, len = chars.length; i < len; i++) {
+        openIndex = open.indexOf(chars[i]);
+        closeIndex = close.indexOf(chars[i]);
+        if (openIndex !== -1) {
+            // Нашли открывающую скобку. Помещаем ее в стек (п.2).
+            if (closeIndex !== -1 && arr[closeIndex] > 0) {
+                openIndex = stack.pop();
+                count = 0;
+                arr[closeIndex] = count;
+                if (closeIndex !== openIndex) {
+                    return false;
+                }
+                continue;
+            } else if (closeIndex !== -1 && arr[closeIndex] === 0) {
+                stack.push(openIndex);
+                count++;
+                arr[closeIndex] = count;
+                continue;
+            } else {
+                stack.push(openIndex);
+                continue;
+            }
+        }
+
+
+        if (closeIndex !== -1) {
+            // Нашли закрывающую скобку. Проверяем ее соответствие открывающей (п.3).
+            openIndex = stack.pop();
+            if (closeIndex !== openIndex) {
+                return false;
+            }
+        }
+    }
+
+    // Проверяем дисбаланс открытых/закрытых скобок (п.5).
+    if (stack.length !== 0) {
+        return false;
+    }
+
+    return true;
 }
